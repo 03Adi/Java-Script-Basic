408fd0d0b2556684966627a61f3d2b55c606cf2a
03Adi <adityapandey03012003@gmail.com>
2024-02-20 07:40:16 +0000

    #Day20

---
+++ class.js
@@ -0,0 +1,15 @@
+// class Railway{
+//   submit(){
+//     alert("form submitted")
+//   }
+//   cancle(){
+//     alert("form cancelled")
+//   }
+// }
+// let aditya=new Railway()
+// let prakash=new Railway()
+// aditya.submit()
+// prakash.submit()
+// prakash.cancle()
+
+console.log("Aditya")
