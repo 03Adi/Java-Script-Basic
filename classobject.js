adb320c4d5be3c4de0ebba3c431758d09066e319
adityapandey03012003@gmail.com <104114546+03Adi@users.noreply.github.com>
2024-02-20 13:23:34 +0530

    #Day20

---
+++ classobject.js
@@ -0,0 +1,14 @@
+class Railway{
+    submit(){
+        alert("form submitted")
+    }
+    cancel(){
+        alert("form cancelled")
+    }
+}
+let aditya=new Railway()
+let prakash=new Railway()
+
+aditya.submit()
+prakash.submit()
+prakash.cancel()
